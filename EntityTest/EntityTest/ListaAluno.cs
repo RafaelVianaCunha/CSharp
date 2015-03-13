f2927b07c8345df0d6dc649b90472a2b185d032d
Rafael Viana <rafael.cunha@al.infnet.edu.br>
2015-03-13 17:27:55 -0300

    #2 - adicionado ListarAluno.cs

---
+++ EntityTest/EntityTest/ListaAluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityTest
+{
+ public static class ListaAluno
+    {
+         public static void Listar()
+       {
+           using (var ctx = new BancoContext())
+           {
+               var consultar = from a in ctx.Alunos select a;
+
+               foreach(Aluno a in consultar)
+               {
+                   Console.WriteLine("Nome: "+a.Nome);
+                   Console.WriteLine("Idade: " + a.idade);
+                   Console.WriteLine("Logradouro: " + a.Endereco.Lougradouro);
+                   Console.WriteLine("Numero: " + a.Endereco.Numero);
+                   Console.WriteLine("CEP: " + a.Endereco.CEP);
+                   Console.WriteLine("-------------------------");
+               }
+
+               
+               
+           }
+       }
+    }
+}
